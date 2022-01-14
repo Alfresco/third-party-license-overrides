1fc1f401845e3424ed606c90bb782fb026adf338
Tom Page <thomas.page@alfresco.com>
2022-01-14 11:54:30 +0000

    ACS-2072 Add E2E test for processing a war file.

--- test/thirdPartyLicenseCSVCreatorTests/projects/specifiedLicensePreferences/command.sh
+++ test/thirdPartyLicenseCSVCreatorTests/projects/specifiedLicensePreferences/command.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=`dirname "$0"`
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # For some reason we prefer GPL-3.0 in this project.
 python3 "$1" --project "$SCRIPT_DIR/project" --desired="GPL-3.0|CDDL-1.0|EPL-2.0" --version "1.2.3" --output "$SCRIPT_DIR/actual"
---
+++ test/thirdPartyLicenseCSVCreatorTests/projects/warProject/command.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Create a temporary war file from the project directory.
+cd "$SCRIPT_DIR/project"
+zip -q "$SCRIPT_DIR//project.war" -r *
+cd "$SCRIPT_DIR"
+
+# Run the generator script.
+python3 "$1" --zip "$SCRIPT_DIR/project.war" --version "1.2.3" --output "$SCRIPT_DIR/actual"
+
+# Tidy up.
+rm "$SCRIPT_DIR/project.war"
