1a3364f9fb662a3d41db78ecabf8bb909cd64a96
Tom Page <thomas.page@alfresco.com>
2022-03-04 15:29:32 +0000

    Generate extra license file from pre-commit.

--- licenseProcessor.py
+++ licenseProcessor.py
@@ -2,18 +2,28 @@
 # -*- coding: utf-8 -*-
 
 includes = 'includedLicenses.txt'
+extra_includes = {'includedLicensesPlusAspose.txt': ['Aspose EULA']}
 merges = 'licenseMerges.txt'
 overrides = 'override-THIRD-PARTY.properties'
 
 # Sort the includes file.
 with open(includes) as f:
-	lines = f.readlines()
+	includes_lines = f.readlines()
 with open(includes, 'w') as f:
-	lines = sorted(lines, key=str.casefold)
-	for line in lines:
+	includes_lines = sorted(includes_lines, key=str.casefold)
+	for line in includes_lines:
 		line = line.strip()
 		if line:
 			f.write(line + '\n')
+
+# Generate extra includes files based on the original.
+for extra_file, extra_licenses in extra_includes.items():
+	with open(extra_file, 'w') as f:
+		extra_file_lines = sorted(includes_lines + extra_licenses, key=str.casefold)
+		for line in extra_file_lines:
+			line = line.strip()
+			if line:
+				f.write(line + '\n')
 
 # Sort the merges file.
 with open(merges) as f:
